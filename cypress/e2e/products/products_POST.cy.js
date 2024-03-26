d3aa62652c4d30c6f8e8900430c7cbec31f67bdf
ApoorvaLingashettar <apoorva@shipwell.com>
2024-03-26 13:00:11 -0500

    Update products_POST.cy.js

--- cypress/e2e/products/products_POST.cy.js
+++ cypress/e2e/products/products_POST.cy.js
@@ -89,38 +89,3 @@ describe("Tests for POST calls on products", () => {
   });
 });
 
-describe("tests for PUT/PATCH calls", () => {
-
-  it("update existing product - PUT", () => {
-    const testproductId = 1;
-    const testTitle = "iPhone Galaxy +1";
-    cy.request({
-      url: `products/${testproductId}`,
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        title: testTitle,
-      }),
-    }).then((response) => {
-      expect(response.status).eq(200);
-      expect(response.body).to.have.property("id", testproductId);
-      expect(response.body).to.have.property("title", testTitle);
-    });
-  }),
-
-  it("update existing product - PATCH - NEGATIVE", () => {
-    const inValidTestproductId = 0;
-    const testTitle = "iPhone Galaxy +1";
-    cy.request({
-        url: `products/${inValidTestproductId}`,
-        method: "PUT",
-        headers: { "Content-Type": "application/json" },
-        failOnStatusCode: false,
-        body: JSON.stringify({
-            title: testTitle,
-     })
-    }).then((response) => {
-    expect(response.status).eq(404);
-    });
-  });
-});
